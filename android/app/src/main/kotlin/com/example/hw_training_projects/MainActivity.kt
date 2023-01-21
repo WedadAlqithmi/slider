f9db7ca6db4ac25b5ef69ef92fa56f6ca700e1bb
wedad <wedadalqithmi@gmail.com>
2023-01-21 20:30:20 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/hw_training_projects/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hw_training_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar:
+              AppBar(title: Text("wh_training projects "), centerTitle: true),
+          body: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 200,
+                  color: Colors.purple,
+                ),
+              ),
+          ],
+        )
+      )
+    )
+  );
+}
